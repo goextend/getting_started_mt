98bd3f3fc34fabd939bddc0ee61139f92fb06d33
Raymond Camden <raymondcamden@gmail.com>
2018-08-30 15:59:23 -0500

    oops

--- generateToken.js
+++ generateToken.js
@@ -1,7 +1,7 @@
 const rp = require('request-promise');
 
 const extend_host = 'https://sandbox.auth0-extend.com';
-const extend_token = 'eyJhbGciOiJIUzI1NiIsImtpZCI6IjIifQ.eyJqdGkiOiIzZmRkMjFkNDAyMjM0NTJjYWNkMTljZDExZTRkMTY1ZSIsImlhdCI6MTUzNTA1Mjg3MSwiY2EiOlsiY2RjOTQ5MTQ1Njk1NDA5N2E0ZWE0ZDA2YjI1ZWI1YTMiXSwiZGQiOjIsInRlbiI6Ii9eZXh0LTQxZWJlZWM4OWQ5ZGFlMmYxNDQzMzBjOTg5MGIyOThmLVswLTFdJC8iLCJzaWQiOiJleHRlbmQtZnJlZW1pdW06YXV0aDB8NWI3ZjBjNDVkZDQ0YTkyYTI1OWMxN2Y5In0.B7AjUlSy0zOLD35R98Qfp6iFG0ZSo0u87n482xTLilw';
+const extend_token = '';
 
 let options = {
 	uri:`${extend_host}/api/tokens/issue`,
